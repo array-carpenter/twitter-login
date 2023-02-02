5287bfba99fc4b5cde66e22ffc92c9d36cc3fa5b
Ray Carpenter <55845213+array-carpenter@users.noreply.github.com>
2023-02-02 14:43:47 -0500

    Created xpaths to variables

--- twitter-login.py
+++ twitter-login.py
@@ -8,26 +8,29 @@ driver = webdriver.Chrome('Your Local Web Driver File Path')
 username = 'Insert Your Twitter Username or Email for Login'
 password = 'Insert Twitter Password, encryption recommended'
 
+# gather xpaths needed for login
+sign_in = "/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/a/div/span/span"
+username_box = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input"
+next_button = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div"
+password_box = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
+login_button = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div"
+
 def log_into_twitter(username, password):
     driver.get('twitter.com')
     time.sleep(5)
-    # click sign in button
-    driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/a/div/span/span").click()
-    # click username box
-    driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input").click()
-    # enter username
+    driver.find_element_by_xpath(sign_in).click()
+    time.sleep(5)
+    driver.find_element_by_xpath(username_box).click()
+    time.sleep(5)
     driver.send_keys(username)
     time.sleep(5)
-    # click next
-    driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div").click()
-    # click password box
-    driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input").click()
+    driver.find_element_by_xpath(next_button).click()
     time.sleep(5)
-    # enter password
+    driver.find_element_by_xpath(password_box).click()
+    time.sleep(5)
     driver.send_keys(password)
     time.sleep(5)
-    #click login
-    driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div").click()
+    driver.find_element_by_xpath(login_button).click()
     # successful login
 
 
