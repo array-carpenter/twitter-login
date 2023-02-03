fc6ac26937c2ba9292445881d25ce11e327da63a
Ray Carpenter <55845213+array-carpenter@users.noreply.github.com>
2023-02-03 16:01:00 -0500

    put everything in a class

--- twitter-login.py
+++ twitter-login.py
@@ -4,11 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome('Your Local Web Driver File Path')
-
-username = 'Insert Your Twitter Username or Email for Login'
-password = 'Insert Twitter Password, encryption recommended'
-
 # gather xpaths needed for login
 sign_in = "/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/a/div/span/span"
 username_box = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input"
@@ -16,24 +11,37 @@ next_button = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2
 password_box = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
 login_button = "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div"
 
-def log_into_twitter(username, password):
-    driver.get('twitter.com')
-    time.sleep(5)
-    driver.find_element_by_xpath(sign_in).click()
-    time.sleep(5)
-    driver.find_element_by_xpath(username_box).click()
-    time.sleep(5)
-    driver.send_keys(username)
-    time.sleep(5)
-    driver.find_element_by_xpath(next_button).click()
-    time.sleep(5)
-    driver.find_element_by_xpath(password_box).click()
-    time.sleep(5)
-    driver.send_keys(password)
-    time.sleep(5)
-    driver.find_element_by_xpath(login_button).click()
-    # successful login
+
+class twitter_login:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome('Your Local Web Driver File Path')
+
+    def log_into_twitter(self):
+        self.driver.get('https://twitter.com/')
+        time.sleep(5)
+        self.driver.find_element_by_xpath(sign_in).click()
+        time.sleep(5)
+        self.driver.find_element_by_xpath(username_box).click()
+        time.sleep(5)
+        self.driver.send_keys(self.username)
+        time.sleep(5)
+        self.driver.find_element_by_xpath(next_button).click()
+        time.sleep(5)
+        self.driver.find_element_by_xpath(password_box).click()
+        time.sleep(5)
+        self.driver.send_keys(self.password)
+        time.sleep(5)
+        self.driver.send_keys(Keys.RETURN)
+        # successful login
+        time.sleep(10)
+    
+    def close_window(self):
+        self.driver.quit()
 
 
 
-log_into_twitter(username, password)
+login = twitter_login("insert usernamw", "insert password")
+login.login()
+login.close()
